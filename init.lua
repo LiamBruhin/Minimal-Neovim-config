13afa2a98f308479e2f572119a7b5225739bcd88
Liam Bruhin <lpbruhin@gmail.com>
2025-12-22 21:39:42 -0500

    lsp and other stuff probably

--- init.lua
+++ init.lua
@@ -17,7 +17,8 @@ vim.o.scrolloff = 15
 vim.o.colorcolumn = "110"
 vim.o.swapfile = false
 vim.o.winborder = "rounded"
-vim.o.textwidth = 75
+vim.o.signcolumn = "yes"
+--vim.o.textwidth = 75
 
 vim.o.makeprg = 'build.bat'
 
@@ -27,20 +28,28 @@ vim.opt.cino = { "t0", "(0" }
 vim.pack.add({
     { src = 'https://github.com/ellisonleao/gruvbox.nvim.git', name = 'gruvbox' },
     { src = 'https://github.com/echasnovski/mini.pick.git' },
-    { src = 'https://github.com/nvim-lualine/lualine.nvim.git' }
+    { src = 'https://github.com/nvim-lualine/lualine.nvim.git' },
+    { src = 'https://github.com/mason-org/mason.nvim.git'},
+    { src = 'https://github.com/neovim/nvim-lspconfig' },
+    { src = 'https://github.com/hrsh7th/cmp-nvim-lsp.git' },
+    { src = 'https://github.com/saadparwaiz1/cmp_luasnip.git' },
+    { src = 'https://github.com/L3MON4D3/LuaSnip.git', version = 'v2.*', run = 'make install_jsregexp' },
+    { src = 'https://github.com/rafamadriz/friendly-snippets' },
+    { src = 'https://github.com/hrsh7th/nvim-cmp.git' }
 })
 
-  local win_config = function()
+local win_config = function()
     local height = math.floor(0.2 * vim.o.lines)
     local width = math.floor(0.4 * vim.o.columns)
     return {
-      anchor = 'NW', height = height, width = width,
-      row = math.floor(0.5 * (vim.o.lines - height)),
-      col = math.floor(0.5 * (vim.o.columns - width)),
+        anchor = 'NW', height = height, width = width,
+        row = math.floor(0.5 * (vim.o.lines - height)),
+        col = math.floor(0.5 * (vim.o.columns - width)),
     }
-  end
+end
+
 require("mini.pick").setup({
-   window = { config = win_config }, 
+    window = { config = win_config },
 })
 vim.keymap.set('n', '<C-p>', ':Pick files<cr>')
 vim.keymap.set('n', '<C-h>', ':Pick help<cr>')
@@ -62,8 +71,60 @@ require("gruvbox").setup({
 
 require('lualine').setup()
 
-vim.cmd.colorscheme('gruvbox')
+require("mason").setup({
+    ui = {
+        icons = {
+            package_installed = "✓",
+            package_pending = "➜",
+            package_uninstalled = "✗"
+        }
+    }
+})
 
+-- LSP
+vim.lsp.enable('lua_ls', {
+    settings = {
+    },
+})
+
+vim.lsp.enable('clangd')
+
+
+vim.lsp.enable('jdtls')
+
+-- Snippets
+local cmp = require("cmp")
+require("luasnip.loaders.from_vscode").lazy_load()
+
+cmp.setup({
+    snippet = {
+        expand = function(args)
+            require("luasnip").lsp_expand(args.body)
+        end,
+    },
+    window = {
+        completion = cmp.config.window.bordered(),
+        documentation = cmp.config.window.bordered(),
+    },
+    mapping = cmp.mapping.preset.insert({
+        ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+        ["<C-f>"] = cmp.mapping.scroll_docs(4),
+        ["<C-Space>"] = cmp.mapping.complete(),
+        ["<C-e>"] = cmp.mapping.abort(),
+        ["<CR>"] = cmp.mapping.confirm({ select = true }),
+    }),
+    sources = cmp.config.sources({
+        { name = "nvim_lsp" },
+        { name = "luasnip" },
+    }, {
+        { name = "buffer" },
+    }),
+})
+
+
+
+-- colors
+vim.cmd.colorscheme('gruvbox')
 
 -- TODO(liam): Wow!
 -- NOTE(liam): Look at that!
