6870c45bfb06d10aed12b9a644da83ef5bda95e5
CS Student <lpbruhin@gmail.com>
2026-01-30 11:13:07 -0500

    bugfix

--- init.lua
+++ init.lua
@@ -33,7 +33,7 @@ vim.pack.add({
     { src = 'https://github.com/neovim/nvim-lspconfig' },
     { src = 'https://github.com/hrsh7th/cmp-nvim-lsp.git' },
     { src = 'https://github.com/saadparwaiz1/cmp_luasnip.git' },
-    { src = 'https://github.com/L3MON4D3/LuaSnip.git', version = 'v2.*', run = 'make install_jsregexp' },
+    { src = 'https://github.com/L3MON4D3/LuaSnip.git', run = 'make install_jsregexp' },
     { src = 'https://github.com/rafamadriz/friendly-snippets' },
     { src = 'https://github.com/hrsh7th/nvim-cmp.git' }
 })
